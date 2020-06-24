1c9e3649f208cc5b323d2f330b21a536cfcc058a
96409 <964097833@qq.com>
2020-06-24 09:07:25 +0800

    redis实现消息通信（发送端）

---
+++ boot_07_2_send/src/main/java/cn/yqd/task/MessageSender.java
@@ -0,0 +1,24 @@
+package cn.yqd.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableScheduling //开启定时器功能
+@Component
+public class MessageSender {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Scheduled(fixedRate = 2000) //间隔2s 通过StringRedisTemplate对象向redis消息队列chat频道发布消息
+    public void sendMessage() {
+        stringRedisTemplate.convertAndSend("myChannel",
+                new SimpleDateFormat("yyyy-MM-dd hh-mm-ss").format(new Date()));
+    }
+}
